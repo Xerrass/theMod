bf36ae5c010248bf7dcf88e4c3bd6015bddef96f
Jonatan Mohs <jonathan.mohs@famohs.de>
2015-10-19 18:43:33 +0200

    added solid resources and new types of fluid

---
+++ theMod_0.0.1/prototypes/resources/solid/items.lua
@@ -0,0 +1,21 @@
+data:extend(
+{
+	{
+		type = "item",
+		name = "sand",
+		icon = "__theMod__/graphics/icons/sand.png",
+		flags = {"goes-to-main-inventory"},
+		subgroup = "intermediat-items",
+		order = "a[the-mod-resources]-e[sand]",
+		stack_size = 200
+	},
+	{
+		type = "item",
+		name = "garvel",
+		icon = "__theMod__/graphics/icons/gravel.png",
+		flags = {"goes-to-main-inventory"},
+		subgroup = "intermediat-items",
+		order = "a[the-mod-resources]-e[gravel]",
+		stack_size = 200
+	},
+})
